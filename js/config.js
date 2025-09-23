1d714377b48f06152b2e181bb1a0e7911c66a134
Osamaabdalkader <osamaabdalkader100@gmail.com>
2025-09-24 02:17:19 +0300

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,22 +1,22 @@
-// config.js - معدل ومصحح
+// config.js - مصحح بالكامل
 const CONFIG = {
     SUPABASE_URL: 'https://rrjocpzsyxefcsztazkd.supabase.co',
     SUPABASE_KEY: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJyam9jcHpzeXhlZmNzenRhemtkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTgyOTEzMTgsImV4cCI6MjA3Mzg2NzMxOH0.TvUthkBc_lnDdGlHJdEFUPo4Dl2n2oHyokXZE8_wodw',
     
     PAGE_FILES: {
         'home': 'home.html',
-        'publish': 'add-post.html',
+        'publish': 'add-post.html',  // تم التصحيح من 'publish' إلى 'add-post.html'
         'login': 'login.html',
         'register': 'register.html',
         'profile': 'profile.html',
-        'post-details': 'post-detail.html',
+        'post-details': 'post-detail.html', // تأكد من أن الملف اسمه post-detail.html وليس post-details.html
         'notifications': 'notifications.html',
         'groups': 'groups.html',
         'cart': 'cart.html',
         'support': 'support.html'
     },
     
-    MAX_IMAGE_SIZE: 5 * 1024 * 1024, // 5MB
+    MAX_IMAGE_SIZE: 5 * 1024 * 1024,
     SUCCESS_MESSAGE_DURATION: 3000
 };
 
